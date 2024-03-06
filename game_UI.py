1198cc85650799af1b443c3c9b3d796bc0cc7454
INF-Alex <132814338+INF-Alex@users.noreply.github.com>
2024-03-06 20:04:26 +0800

    Update game_UI.py first hand

--- game_UI.py
+++ game_UI.py
@@ -5,6 +5,9 @@ import sys
 WIDTH, HEIGHT = 600, 600
 BOARD_SIZE = 3
 SQUARE_SIZE = WIDTH // BOARD_SIZE
+TICK = 1
+CROSS = -1
+EMPTY = 0
 
 # Colors
 WHITE = (255, 255, 255)
@@ -20,13 +23,31 @@ def draw_chess_board(screen):
     for y in [WIDTH/3,WIDTH*2/3]:
         pygame.draw.line(screen, BLACK, (0, y), (HEIGHT, y), 2)
 
-def draw_cross(screen, row, col):
-    x = row * SQUARE_SIZE + SQUARE_SIZE // 2
-    y = col * SQUARE_SIZE + SQUARE_SIZE // 2
-    pygame.draw.circle(screen, C1, (x, y), SQUARE_SIZE // 3)
+def draw_cross(screen, MAP):
+    for row in range(3):
+        for col in range(3):
+            if MAP[row][col]:
+                x = row * SQUARE_SIZE + SQUARE_SIZE // 2
+                y = col * SQUARE_SIZE + SQUARE_SIZE // 2
+                if MAP[row][col] == 1:
+                    pygame.draw.circle(screen, C1, (x, y), SQUARE_SIZE // 5)
+                else:
+                    pygame.draw.circle(screen, C2, (x, y), SQUARE_SIZE // 5)
 
 def get_mouse_cell(position):
     return [position[0] // SQUARE_SIZE, position[1] // SQUARE_SIZE]
+
+def check(MAP):
+    for player in [TICK,CROSS]:
+        if (any([all([MAP[x][y] == player for y in range(3)]) for x in range(3)]) 
+            or any([all([MAP[x][y] == player for x in range(3)]) for y in range(3)])):
+            return player
+        if all([MAP[x][x] == player for x in range(3)]) or all([MAP[x][2-x] == player for x in range(3)]):
+            return player
+    if all([MAP[x][y] != 0 for x in range(3) for y in range(3)]):
+        return 0    # 平局
+    return None
+
 
 def game_UI():
     pygame.init()
